420efcd1a0656d9a44b71c353fa91b9074b49d94
Curtis Hodgson <39945904+CurtisDH@users.noreply.github.com>
2021-04-26 01:42:14 +1000

    - Value from each thread is put into a total int. -> Significantly slower

--- PrimeNumberFinder.cs
+++ PrimeNumberFinder.cs
@@ -2,22 +2,21 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreadTesting
 {
     static class PrimeNumberFinder
     {
-        //TODO Figure out how to return a value from the thread
-        private static int _endingNum = 500;
-        //private static int _total;
-        private static readonly List<int> NumFound = new List<int>();
+        private static int _endingNum = 500000;
+        private static int _total;
 
         static void Main(string[] args)
         {
             Console.WriteLine($"Processor count: {Environment.ProcessorCount}");
             CreateThreads(Environment.ProcessorCount);
             Console.ReadLine();
-            //Console.WriteLine("Total:" + NumFound.Count);
+            Console.WriteLine("Total:" + _total);
         }
 
         private static void CreateThreads(int numberOfThreads)
@@ -30,12 +29,15 @@ namespace MultiThreadTesting
                 Console.WriteLine(i);
                 var endNum = _endingNum * i;
                 var threadNumber = i;
-                Thread thread = new Thread(t => FindPrimesTask(endNum - _endingNum, endNum, threadNumber));
-                thread.Start();
+                Task<int> task = Task<int>.Factory.StartNew(() => FindPrimesTaskInt(endNum - _endingNum, endNum, threadNumber));
+                _total += task.Result;
+                //Thread thread = new Thread(t => FindPrimesTask(endNum - _endingNum, endNum, threadNumber));
+                //thread.Start();
             }
 
             stopwatch.Stop();
-            Console.WriteLine($"time since all thread creation:{stopwatch.ElapsedMilliseconds}ms");
+            //Console.WriteLine($"time since all thread creation:{stopwatch.ElapsedMilliseconds}ms");
+            Console.WriteLine($"time since all thread completion:{stopwatch.ElapsedMilliseconds}ms");
         }
 
         private static void FindPrimesTask(int start, int end,int threadNumber)
@@ -73,5 +75,47 @@ namespace MultiThreadTesting
                 $"###THREAD {threadNumber}###Time Elapsed: {stopwatch.ElapsedMilliseconds}ms" +
                 $" Found: {_foundPrimes.Count} Started from: {start} Ended at: {end}");
         }
+        
+        private static int FindPrimesTaskInt(int start, int end,int threadNumber)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            bool _prime = true;
+            List<int> _foundPrimes = new List<int>();
+            for (int i = start; i < end; i++)
+            {
+                _prime = true;
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+
+                for (int t = 2; t <= i / 2; t++)
+                {
+                    if (i % t == 0)
+                    {
+                        _prime = false;
+                        break;
+                    }
+                }
+
+                if (_prime)
+                {
+                    _prime = false;
+                    _foundPrimes.Add(i);
+                    //NumFound.Add(i);// need to figure out how to return a value safely
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(
+                $"###THREAD {threadNumber}###Time Elapsed: {stopwatch.ElapsedMilliseconds}ms" +
+                $" Found: {_foundPrimes.Count} Started from: {start} Ended at: {end}");
+            return _foundPrimes.Count;
+
+        }
+        
     }
+    
+    
+    
 }
